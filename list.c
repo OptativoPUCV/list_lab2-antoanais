9729e3fb5965760b58887ee3f003c1cae4b13b5e
Replit user <replituser@example.com>
2021-03-26 00:13:43 +0000

    new try

--- list.c
+++ list.c
@@ -137,7 +137,7 @@ void * popCurrent(List * list) {
     list -> current -> prev = NULL;
     list-> head = list -> current;
   }
-  return (void*) list->head->data;
+  return (void*) list;
 }
 
 void cleanList(List * list) {
